ea5e7a5abebda98253f53879c83a36dc9658778c
郑康霖 <1255964530@qq.com>
2018-11-29 22:33:37 +0800

    创建UserAdd页面组件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter, Route, Link } from 'react-router-dom'
+import UserAddPage from './pages/UserAdd'
 
-ReactDOM.render((
-  <div>Hello React!</div>
-), document.getElementById('app'))
+ReactDOM.render(
+  <HashRouter>
+    <Route exact path="/user/add" component={UserAddPage} />
+  </HashRouter>,
+  document.getElementById('app')
+)
